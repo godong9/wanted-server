e6b33f9667a72b7e440dc5b8e1ce3ff5be83ea8d
Donghyun Go <godong9@gmail.com>
2015-08-08 12:44:31 +0900

    Update items

--- server/controllers/Item.js
+++ server/controllers/Item.js
@@ -7,22 +7,29 @@ function ItemCtrl() {
 
 }
 
-/**
- *
- * @param req
- * @param res
- * @returns {*}
- */
 ItemCtrl.getItems = function(req, res) {
-    var errors, criteria, projection;
-    req.checkQuery('location.lat', 'Invalid location').notEmpty();
-    req.checkQuery('location.lng', 'Invalid location').notEmpty();
+    var errors, criteria, projection, options;
+    req.checkQuery('location.lat', 'Invalid location.lat').notEmpty();
+    req.checkQuery('location.lng', 'Invalid location.lng').notEmpty();
+    req.checkQuery('pageNum', 'Invalid pageNum').notEmpty();
+    req.checkQuery('perPage', 'Invalid perPage').notEmpty();
     errors = req.validationErrors();
     if (errors) return res.status(400).send(Result.ERROR(errors));
     criteria = {};
+    criteria["location.loc"]= {
+        $near : {
+            $geometry : {
+                type : "Point" ,
+                coordinates : [ req.query.location.lng, req.query.location.lat ]
+            }
+        }
+    };
     projection = { itemImgUrl:1, category:1, name:1, location:1, lostDate:1 };
-    options = {};
-    Item.getItems({}, function(err, docs) {
+    options = {
+        skip: parseInt(req.query.pageNum) * parseInt(req.query.perPage),
+        limit: parseInt(req.query.perPage)
+    };
+    Item.getItems(criteria, projection, options, function(err, docs) {
         if (err) return res.status(400).send(Result.ERROR(err));
         res.status(200).send(Result.SUCCESS(docs));
     });
--- server/routes/items.js
+++ server/routes/items.js
@@ -2,6 +2,26 @@ var express = require('express');
 var router = express.Router();
 var itemCtrl = require('../controllers/Item');
 
+/**
+ * ---------
+ * ## **GET items**
+ *  - 현재 위치 주변 item 목록 가져오는 API
+ *
+ * ### URL: /item/list
+ * ### TYPE: POST
+ *
+ * @param {JSONObject} location: 현재 위치 데이터
+ * @param {String} location.lat: 현재 위치 위도
+ * @param {String} pageNum: 페이지 번호 (0부터 시작)
+ * @param {String} perPage: 페이지당 가져올 개수
+ *
+ * @example
+ *  REQUEST
+ *   - GET /item/list?pageNum=0&perPage=10&location={"lat":37.497942,"lng":127.02762099999995}
+ *  RESPONSE
+ *   -
+ */
+router.get('/item/list', itemCtrl.getItems);
 
 
 module.exports = router;
